0856d3faac1744140ffd1dc7685e71c5944de35e
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-22 12:14:02 -0300

    feat: viewing data with EJS

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var conn = require('../inc/db');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  conn.query('SELECT * FROM tb_menus ORDER BY title', (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { 
+        title: 'Restaurante Saboroso', 
+        menus: results 
+      });
+    }
+  })
+
+  
 });
 
 module.exports = router;
