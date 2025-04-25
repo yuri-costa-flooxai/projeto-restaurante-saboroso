cc0d1cb6e6f78ca84f57a6fe743671cf066ad195
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-25 13:42:31 -0300

    feat: dashboard mysql consulting

--- saboroso/inc/admin.js
+++ saboroso/inc/admin.js
@@ -1,5 +1,22 @@
 module.exports = {
 
+    dashboard() {
+
+        return new Promise((resolve, reject) => {
+
+            connect.query(`SELECT
+    (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
+    (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
+    (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
+    (SELECT COUNT(*) FROM tb_users) AS nrusers;`, (err, results) => {
+                if (err) {
+                     reject(err);
+                }
+                resolve(results[0]);
+            });
+        });
+    },
+
     getParams(req, params) {
         return Object.assign({}, {
             date:{},
